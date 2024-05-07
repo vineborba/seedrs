b7142152a869459478d205b399691906073d2fbb
Vinicius de Borba <vinicius.borba@hotmail.com>
2024-05-07 00:28:11 -0300

    feat: added some validation for package manager selection

--- bootstrapper/src/package_managers.rs
+++ bootstrapper/src/package_managers.rs
@@ -1,4 +1,6 @@
+use anyhow::{bail, Result};
 use clap::ValueEnum;
+use which::which;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, ValueEnum)]
 pub enum PackageManagers {
@@ -14,5 +16,13 @@ impl PackageManagers {
             PackageManagers::Pnpm => String::from("pnpm"),
             PackageManagers::Yarn => String::from("yarn"),
         }
+    }
+
+    pub fn check_if_availabe(&self) -> Result<&Self> {
+        if which(self.executable_name()).is_err() {
+            bail!("Package manager isn't available for use!");
+        }
+
+        Ok(self)
     }
 }
